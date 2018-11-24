f2d7d20a9a7ef359ff0c5e14ae2dc833ca2c80da
aminfaruq <aminfaruk.fa@gmail.com>
2018-11-24 09:28:47 +0800

    di perbarui

---
+++ app/src/main/java/id/co/aminfaruq/conventersuhu/SplashScreen.java
@@ -0,0 +1,31 @@
+package id.co.aminfaruq.conventersuhu;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+    private int waktu_loading=4000;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                //setelah loading maka akan langsung berpindah ke home activity
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },waktu_loading);
+
+    }
+}
